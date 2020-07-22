e67b950205f26998d430153401346e1ae56b6328
codetimer <zghnzch@hotmail.com>
2020-07-22 12:19:51 +0800

    更新 至 netty4.1.39

--- src/main/java/io/netty/example/udt/echo/bytes/ByteEchoClient.java
+++ src/main/java/io/netty/example/udt/echo/bytes/ByteEchoClient.java
@@ -14,7 +14,6 @@
  * under the License.
  */
 package io.netty.example.udt.echo.bytes;
-
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -26,7 +25,6 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.concurrent.DefaultThreadFactory;
 
 import java.util.concurrent.ThreadFactory;
-
 /**
  * UDT Byte Stream Client
  * <p>
@@ -36,36 +34,29 @@ import java.util.concurrent.ThreadFactory;
  * server.
  */
 public final class ByteEchoClient {
-
-    static final String HOST = System.getProperty("host", "127.0.0.1");
-    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
-    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
-
-    public static void main(String[] args) throws Exception {
-        // Configure the client.
-        final ThreadFactory connectFactory = new DefaultThreadFactory("connect");
-        final NioEventLoopGroup connectGroup = new NioEventLoopGroup(1,
-                connectFactory, NioUdtProvider.BYTE_PROVIDER);
-        try {
-            final Bootstrap boot = new Bootstrap();
-            boot.group(connectGroup)
-                    .channelFactory(NioUdtProvider.BYTE_CONNECTOR)
-                    .handler(new ChannelInitializer<UdtChannel>() {
-                        @Override
-                        public void initChannel(final UdtChannel ch)
-                                throws Exception {
-                            ch.pipeline().addLast(
-                                    new LoggingHandler(LogLevel.INFO),
-                                    new ByteEchoClientHandler());
-                        }
-                    });
-            // Start the client.
-            final ChannelFuture f = boot.connect(HOST, PORT).sync();
-            // Wait until the connection is closed.
-            f.channel().closeFuture().sync();
-        } finally {
-            // Shut down the event loop to terminate all threads.
-            connectGroup.shutdownGracefully();
-        }
-    }
+	static final String HOST = System.getProperty("host", "127.0.0.1");
+	static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+	static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+	public static void main(String[] args) throws Exception {
+		// Configure the client.
+		final ThreadFactory connectFactory = new DefaultThreadFactory("connect");
+		final NioEventLoopGroup connectGroup = new NioEventLoopGroup(1, connectFactory, NioUdtProvider.BYTE_PROVIDER);
+		try {
+			final Bootstrap boot = new Bootstrap();
+			boot.group(connectGroup).channelFactory(NioUdtProvider.BYTE_CONNECTOR).handler(new ChannelInitializer<UdtChannel>() {
+				@Override
+				public void initChannel(final UdtChannel ch) throws Exception {
+					ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO), new ByteEchoClientHandler());
+				}
+			});
+			// Start the client.
+			final ChannelFuture f = boot.connect(HOST, PORT).sync();
+			// Wait until the connection is closed.
+			f.channel().closeFuture().sync();
+		}
+		finally {
+			// Shut down the event loop to terminate all threads.
+			connectGroup.shutdownGracefully();
+		}
+	}
 }
