3c84946ceeb900fc609b0d19ac7c2fe453e26805
zchcpywin10 <zghnzch@163.com>
2020-12-28 19:00:45 +0800

    test netty tcp client

--- src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -14,50 +14,91 @@
  * under the License.
  */
 package io.netty.example.echo;
-
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 /**
  * Handler implementation for the echo client.  It initiates the ping-pong
  * traffic between the echo client and server by sending the first message to
  * the server.
  */
+@Slf4j
 public class EchoClientHandler extends ChannelInboundHandlerAdapter {
-
-    private final ByteBuf firstMessage;
-
-    /**
-     * Creates a client-side handler.
-     */
-    public EchoClientHandler() {
-        firstMessage = Unpooled.buffer(EchoClient.SIZE);
-        for (int i = 0; i < firstMessage.capacity(); i ++) {
-            firstMessage.writeByte((byte) i);
-        }
+  private final ByteBuf firstMessage;
+  public ChannelHandlerContext channelHandlerContextGlobal = null;
+  /**
+   * Creates a client-side handler.
+   */
+  public EchoClientHandler() {
+    firstMessage = Unpooled.buffer(EchoClient.SIZE);
+    for (int i = 0; i < firstMessage.capacity(); i++) {
+      firstMessage.writeByte((byte) i);
     }
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
+  }
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) {
+    System.out.println("1.");
+    System.out.println(ctx);
+    ctx.writeAndFlush(firstMessage);
+    channelHandlerContextGlobal = ctx;
+    System.out.println("1.channelHandlerContextGlobal = " + channelHandlerContextGlobal);
+    Channel channel = ctx.channel();
+    System.out.println("channel = " + channel);
+    /* ================================ */
+    System.out.println("firstMessage = " + firstMessage);
+    /* ================================ */
+    byte[] b = "ABC".getBytes(StandardCharsets.UTF_8);
+    ByteBuf buf = Unpooled.copiedBuffer(b);
+    channelHandlerContextGlobal.writeAndFlush(buf);
+  }
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    System.out.println("2.");
+    System.out.println(ctx);
+    ctx.write(msg);
+    channelHandlerContextGlobal = ctx;
+    System.out.println("channelHandlerContextGlobal = " + channelHandlerContextGlobal);
+    /* ================================ */
+    try {
+      // https://www.cnblogs.com/shoshana-kong/p/11228616.html 有示例
+      ByteBuf buf = (ByteBuf) msg;
+      //通过ByteBuf的readableBytes方法可以获取缓冲区可读的字节数，
+      //根据可读的字节数创建byte数组
+      byte[] b = new byte[buf.readableBytes()];
+      //通过ByteBuf的readBytes方法将缓冲区中的字节数组复制到新建的byte数组中
+      buf.readBytes(b);
+      String receiveStrFromByte = ByteBufUtil.hexDump(b);
+      System.out.println("receiveStrFromByte = " + receiveStrFromByte);
     }
-
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(msg);
+    catch (Exception e) {
+      log.error(e.getMessage(), e);
     }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-       ctx.flush();
+    /* ================================ */
+  }
+  @Override
+  public void channelReadComplete(ChannelHandlerContext ctx) {
+    ctx.flush();
+  }
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    // Close the connection when an exception is raised.
+    cause.printStackTrace();
+    ctx.close();
+  }
+  public static void main(String[] args) {
+    EchoClientHandler clientHandler = new EchoClientHandler();
+    System.out.println("channelHandlerContextGlobal = " + clientHandler.channelHandlerContextGlobal);
+    if(null != clientHandler.channelHandlerContextGlobal){
+      byte[] b = "ABC".getBytes(StandardCharsets.UTF_8);
+      ByteBuf buf = Unpooled.copiedBuffer(b);
+      clientHandler.channelHandlerContextGlobal.writeAndFlush(buf);
     }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+  }
 }
--- src/main/java/io/netty/example/echo/EchoServerHandler.java
+++ src/main/java/io/netty/example/echo/EchoServerHandler.java
@@ -14,31 +14,48 @@
  * under the License.
  */
 package io.netty.example.echo;
-
+import cn.zch.util.LogUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 /**
  * Handler implementation for the echo server.
  */
+@Slf4j
 @Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
-
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(msg);
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ctx.write(msg);
+    /* ================================ */
+    try {
+      // https://www.cnblogs.com/shoshana-kong/p/11228616.html 有示例
+      ByteBuf buf = (ByteBuf) msg;
+      //通过ByteBuf的readableBytes方法可以获取缓冲区可读的字节数，
+      //根据可读的字节数创建byte数组
+      byte[] b = new byte[buf.readableBytes()];
+      //通过ByteBuf的readBytes方法将缓冲区中的字节数组复制到新建的byte数组中
+      buf.readBytes(b);
+      String receiveStrFromByte = ByteBufUtil.hexDump(b);
+      System.out.println("receiveStrFromByte = " + receiveStrFromByte);
     }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-        ctx.flush();
+    catch (Exception e) {
+      log.error(e.getMessage(), e);
     }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+    /* ================================ */
+  }
+  @Override
+  public void channelReadComplete(ChannelHandlerContext ctx) {
+    ctx.flush();
+  }
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    // Close the connection when an exception is raised.
+    cause.printStackTrace();
+    ctx.close();
+  }
 }
